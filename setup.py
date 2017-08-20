c1ac55ad53b2404b8da8a4a83fc98e85648e2850
Sayan Goswami <goswami.sayan47@gmail.com>
2017-08-20 16:37:25 +0530

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pytgress',
   packages = ['pytgress'],
-  version = '1.0.0',
+  version = '1.0.0-rc2',
   description = 'Beautiful progress bars for the CLI',
   author = 'Sayan Goswami',
   author_email = 'goswami.sayan47+pygress@gmail.com',
