28bf370870cffe4a0255c8ef68faa659a93e40b3
Sayan Goswami <goswami.sayan47@gmail.com>
2017-08-20 20:50:34 +0530

    Version bump

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='pytgress',
     packages=['pytgress'],
-    version='1.0.1',
+    version='1.0.2',
     description='Beautiful progress bars for the CLI',
     author='Sayan Goswami',
     author_email='goswami.sayan47+pygress@gmail.com',
     url='https://github.com/sayan98/pytgress',
-    download_url='https://github.com/sayan98/pytgress/archive/v1.0.1.tar.gz',
+    download_url='https://github.com/sayan98/pytgress/archive/v1.0.2.tar.gz',
     keywords=['progress', 'bar', 'spinner', "loader", "loading"],
     classifiers=[],
 )
